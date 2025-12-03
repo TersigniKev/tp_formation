e1b9483f880fbbbac32f233d9bbb4473cbf00388
TersigniKev <tersignikevin.05@gmail.com>
2025-12-03 17:06:08 +0100

    first commit

---
+++ src/Controller/MembresController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MembresRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MembresController extends AbstractController
+{
+    #[Route('/', name: 'app_membres')]
+    public function index(MembresRepository $membresRepository): Response
+    {
+        $membres = $membresRepository->findAll();
+
+        return $this->render('membres/index.html.twig', [
+            'controller_name' => 'MembresController',
+            'membres' => $membres
+        ]);
+    }
+
+
+}
